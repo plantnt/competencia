971f0b6373d1c408d049c6a4c189a0568281b784
plantnt <godzillagmk2204@gmail.com>
2024-06-06 11:42:05 -0500

    new file:   inicia_sesion.php

---
+++ inicia_sesion.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sesión</title>
+</head>
+<body>
+
+<form action="" method="post">
+   
+        <input type="text" name="nombre_equipo" placeholder="nombre de usuario">
+      
+        <input type="text" name="password" placeholder="contraseña">
+<button type="submit">Inciar sesión</button>
+    </form>
+    
+</body>
+</html>
